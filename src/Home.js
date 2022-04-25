0e023270ef25e48d32a2abe36bcb0aff53ae2682
Greg Edmondson <fallabrine@gmail.com>
2022-04-25 10:35:41 +0900

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -4,12 +4,12 @@ class Homepage extends Component {
 
   render() {
     return (
-      <div>
+      <>
         <div style={{width:"100%", height:"20vh", backgroundColor:"black">
           <h1 style={{color:"white"}}>Greg Edmondson</h1>
         </div>
-        <p>This is the Home.js file</p>
-      </div>
+//         <p>This is the Home.js file</p>
+      </>
      );
   }
 }
