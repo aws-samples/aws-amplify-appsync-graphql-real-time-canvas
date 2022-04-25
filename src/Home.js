719b402a3a2620f74ed2f07333fe5919d57060c7
Greg Edmondson <fallabrine@gmail.com>
2022-04-25 12:40:56 +0900

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -9,7 +9,7 @@ class Homepage extends Component {
           <h1 className="HeaderText">Greg Edmondson</h1>
         </div>
       <>
-           <img src="https://amplify-react-draw-devo-04151-deployment.s3.ap-northeast-1.amazonaws.com/cowboy.JPG" />
+           <img height="25%" border-radius="25px" src="https://amplify-react-draw-devo-04151-deployment.s3.ap-northeast-1.amazonaws.com/cowboy.JPG" />
       </>
       </div>
 //         <p>This is the Home.js file</p>
