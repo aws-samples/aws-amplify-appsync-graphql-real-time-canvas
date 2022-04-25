d21d485b0e6be27186bdcbd963eac0b54fa2007c
Greg Edmondson <fallabrine@gmail.com>
2022-04-25 09:05:18 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Canvas from './Canvas';
+import Homepage from './Home';
 import * as serviceWorker from './serviceWorker';
 
 import Amplify from 'aws-amplify'
