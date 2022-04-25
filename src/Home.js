41171aae41953ae03c81d5b804b2ab4ba1c37af5
Greg Edmondson <fallabrine@gmail.com>
2022-04-25 09:01:22 +0900

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
-render() {
-  return (
-    <div>
-      <h1>This is the Home.js file</h1>
-    </div>
-    );
+import React, { Component } from "react";
+
+class Homepage extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>This is the Home.js file</h1>
+      </div>
+     );
+  }
 }
+
+export default Homepage
